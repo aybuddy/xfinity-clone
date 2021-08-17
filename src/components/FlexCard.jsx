b3c670fd102eeee9bd88619b97277fc9e910e621
Aaron Yu <ayybuddyy@gmail.com>
2021-08-17 10:28:41 -0400

    Grab data content from website

---
+++ src/components/FlexCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlexCard = () => {
+  return (
+    <div>
+      <h1>I am a flex card component</h1>
+    </div>
+  );
+};
+
+export default FlexCard;
---
+++ src/data.js
@@ -0,0 +1,148 @@
+export const heroContent = {
+  titleA: 'Get a $300 Visa® Prepaid Card with ',
+  titleB: 'Xfinity Internet and Mobile',
+  desc: 'Savings start with our latest deals on fast speeds. Then get $300 back for a limited time when you connect to the most reliable wireless network nationwide.',
+  buttonText: 'Shop featured deals',
+  extraText1: 'Limited Time',
+  extraText2: 'Pricing & Other Info',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-2598_fall-sale/2.0_Homepage_Hero_Fall_Sale-Desktop-Update-2.jpg',
+};
+
+export const availabilityContent = {
+  image:
+    'https://assets.xfinity.com/assets/dotcom/icons/Icon_location_purple40_88x88.svg',
+  titleA: "Let's find deals and services available in your area",
+  buttonText: 'Check availability',
+};
+
+export const xfiContent = {
+  titleA: 'Powerful and',
+  titleB: 'secure Internet',
+  desc: 'Get WiFi speeds faster than a gig with the xFi Gateway, and count on a reliable connection for all your devices, even when everyone’s online.',
+  buttonText: 'Shop xFi',
+  extraText1:
+    'Gig-speed over WiFi requires Gigabit Internet and compatible xFi Gateway. Gig-speed WiFi is shareable across all the devices in your home. Actual speeds vary.',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/HP_JulyMVE_FullImage_xFiInternet_ImageRight_Desktop2.jpg',
+};
+
+export const flexContent = {
+  titleA: '4K streaming TV box,',
+  titleB: 'free with your Internet',
+  desc: 'Thousands of awesome TV shows, movies, and documentaries. Plus all your favorite apps, all in one place.',
+  buttonText: 'Get Flex free',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-2775_-homepage-flex-and-xm-tiles-return-to-bau/CIX-2775-Homepage-Flex-XM-tiles-return-to-BAU-Desktop-Tiles_Revised.png',
+};
+
+export const mobileContent = {
+  titleA: 'The best price for',
+  titleB: 'Unlimited nationwide 5G',
+  desc: 'The more lines you add, the more value you get. Get unlimited data for $30 a line per month when you get 4 lines.',
+  buttonText: 'Discover Xfinity Mobile',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-2775_-homepage-flex-and-xm-tiles-return-to-bau/CIX-2775-Homepage-Flex-XM-tiles-return-to-BAU-Desktop-Tiles_Revised.png',
+};
+
+export const phoneFinePrint = {
+  desc: 'Best price comparison based upon account with 4 unlimited lines only compared to available 5G pricing plans of top 3 carriers. Xfinity Internet required. Reduced speeds after 20 GB of usage/line. No per line access fees with Xfinity Mobile. Actual savings vary and are not guaranteed.',
+};
+
+export const findDeal = {
+  titleA: 'Get the',
+  titleB: 'perfect plan',
+  titleC: 'for you',
+  buttonText: 'Find my deal',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/icons/Icon_wayfinding_green50_88x88.svg',
+};
+
+export const comparePlans = {
+  titleA: 'Compare plans',
+  titleB: 'in your area',
+  buttonText: 'Shop all plans',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/icons/Icon_pricetag_red40_88x88.svg',
+};
+
+export const rewardsContent = {
+  titleA: 'Our thanks.',
+  titleB: 'Your rewards.',
+  desc: 'Enjoy a mix of special perks and experiences just for being a customer, with Xfinity Rewards.',
+  buttonText: 'Learn more',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/HP_JulyMVE_FullImage_Rewards_ImageLeft_Desktop.png',
+};
+
+export const appContent = {
+  titleA: 'Enjoy ultimate control',
+  titleB: 'of your Internet',
+  desc: 'Personalize your WiFi name and password, assign user profiles, view and control connected devices, and much more.',
+  buttonText: 'Discover the Xfinity app',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/HP_JulyMVE_FullImage_XfinityApp_ImageRight_Desktop.png',
+};
+
+export const benefitsContent = {
+  titleA:
+    "Xfinity is proud to support the Federal Government's Emergency Broadband Benefit",
+  buttonText: 'Learn more',
+  image:
+    'url("https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/HP_JulyMVE_FullImageLarge_EBB_ImageLeft_Desktop.jpg")',
+};
+
+export const manageContent = {
+  titleA: 'Already with Xfinity?',
+  desc: 'Manage your services and discover new ways to stay connected, protected, and entertained.',
+  buttonText: 'Sign in',
+  image:
+    'https://assets.xfinity.com/assets/dotcom/icons/icon_person_purple_outline.svg',
+};
+
+export const servicesContent = {
+  titleA: 'Our services work even better together',
+  internetImage:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_InternetDevice_Desktop.png',
+  internetImageBG:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_InternetBackground_Desktop.png',
+  tvImage:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_VideoDevice_Desktop.png',
+  tvImageBG:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_VideoBackground_Desktop.png',
+  mobileImage:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_MobileDevice_Desktop.png',
+  mobileImageBG:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_MobileBackground_Desktop.png',
+  homeImage:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_CameraDevice_Desktop.png',
+  homeImageBG:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_CameraBackground_Desktop.png',
+  phoneImage:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_PhoneDevice_Desktop.png',
+  phoneImageBG:
+    'https://assets.xfinity.com/assets/dotcom/projects/cix-1976-learn-july-launch/Image_HoverTiles_PhoneBackground_Desktop.png',
+};
+
+export const xfinityFinePrint = {
+  xfp1: "Restrictions apply. Not available in all areas. Services and features vary based on service level. Pricing and programming subject to change. Taxes and fees extra. 30-Day Guarantee applies to one month's recurring service charge and standard installation.",
+  xfpInternetTitle: 'Internet',
+  xfpInternetDesc:
+    'Xfinity xFi is available to Xfinity Internet service customers with a compatible Xfinity Gateway. Not available to Xfinity Prepaid only customers. xFi Advanced Security is available to Xfinity Internet customers with a compatible Xfinity xFi Gateway. Must activate Advanced Security through Xfinity app. Gig-speed over WiFi requires Gigabit Internet and compatible xFi Gateway. Gig-speed WiFi is shareable across all the devices in your home. Actual speeds vary and are not guaranteed. Many factors affect speed, including equipment performance, interference, congestion, and speeds of visited websites. WiFi speeds affected by additional factors, including distance from Gateway, home configuration, personal device capabilities, and others. For factors affecting speed visit www.xfinity.com/networkmanagement.',
+  xfpFlexTitle: 'Flex',
+  xfpFlexDesc:
+    'Not available to current Xfinity Video customers. Requires post-paid subscription to Xfinity Internet, excluding Internet Essentials. Limited to 3 devices. 1 device included, additional devices $5/mo. per device. All devices must be returned when service ends.',
+  xfpPeacocTitle: 'Peacock Premium',
+  xfpPeacocDesc:
+    'Eligible X1 TV box or Flex 4K streaming box required. Service not available without Xfinity Internet or subscription to Digital Starter TV, or equivalent, or above. Peacock Premium has a $4.99/month value. Peacock. © 2021 Peacock TV LLC. Peacock and related marks are trademarks of Peacock LLC. Individual programs, devices and marks are the property of their respective owners.',
+  xfpStreamingTitle: 'Streaming apps on X1 and Flex',
+  xfpStreamingDesc:
+    'Access to Disney+, Netflix, Prime Video, Hulu, YouTube HBO Max, and Paramount+ on Xfinity requires an eligible set-top box with Xfinity TV and Internet service. Disney+, Netflix streaming, Amazon Prime Video, Hulu, HBO Max, and Paramount+ subscriptions required. Viewing uses your Internet service and will count against any Xfinity data plan. Streaming content limited to the U.S. Separate charges apply to On Demand and certain streaming services.',
+  xfpMobileTitle: 'Xfinity Mobile',
+  xfpMobileDesc:
+    'Requires residential post-pay Xfinity Internet. Line limitations may apply. Equip., intl. and roaming charges, taxes and fees, including reg. recovery fees, and other charges extra, and subj. to change. $25/line/mo. charge applies if Xfinity TV, Internet or Voice post-pay services not maintained. Pricing subject to change. In times of congestion, your data may be temporarily slower than other traffic. After 20 GB monthly data use, speeds reduced to a maximum of 1.5Mbps download/750 Kbps upload. A minimum $15.00 charge applies per month, per account for By the Gig lines, regardless of data usage. Charges apply to each GB or partial GB of shared data. No rollover data. For Xfinity Mobile Broadband Disclosures visit: www.xfinity.com/mobile/policies/broadband-disclosures.',
+  xfpOlympics:
+    'The use of Olympic Marks, Terminology and Imagery is authorized by the U.S. Olympic Committee pursuant to Title 36 U.S. Code Section 220506.',
+  xfpCopyright:
+    '© 2021 Comcast. All marks are the property of their respective owners. All rights reserved.',
+};
